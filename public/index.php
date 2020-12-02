37c03f97c6569a9fc94d1807c0606cbc859bfdee
leandrohenrique <leandrohenrique.costasilva@gmail.com>
2020-12-02 13:04:49 -0300

    Criando repositorio para estudo

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Criando Virtual Host</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p><strong>Ok, sua Virtual Host foi configurada corretamente!</strong></p>
+    </body>
+</html>
