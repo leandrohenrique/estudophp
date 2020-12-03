e86abb5960eab8095d3388133393fc158fd85fee
leandrohenrique <leandrohenrique.costasilva@gmail.com>
2020-12-03 16:58:51 -0300

    Listagem dados na tabela

--- public/index.php
+++ public/index.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Criando Virtual Host</title>
+        <title>Exercicio</title>
         <meta charset="utf-8">
     </head>
     <body>
-        <p><strong>Ok, sua Virtual Host foi configurada corretamente!</strong></p>
+        <a href="inserir.php">Cadastrar</a></br></br>
+        <table border="1" width="100%">
+        	<tr>
+	        	<th>Nome</th>
+	        	<th>Email</th>
+	        	<th>Acão</th>
+        	</tr>
+        <?php 
+            require "config.php";
+
+        	$sql = "SELECT * FROM pessoa";
+        	$sql = $con->query($sql);
+
+        	foreach ($sql->fetchAll() as $pessoa) {
+        	?>
+        	<tr>
+	        	<td><?php echo $pessoa['nome'];?></td>
+	        	<td><?php echo $pessoa['email'];?></td>
+	        	<td><a href="editar.php?id=<? echo $pessoa['idpessoa']; ?>">Editar</a> - <a href="excluir.php?id=<? echo $pessoa['idpessoa']; ?>">Excluir</a></td>
+        	</tr>
+        <?php }?>
+        </table>
+
+
     </body>
 </html>
